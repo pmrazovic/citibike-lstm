f45be1f99a0a50fef9265da43b462609eb86175c
Petar Mrazovic <petar.mrazovic@gmail.com>
2018-01-13 01:29:01 +0100

    Processing input data

--- lstm_predictor.py
+++ lstm_predictor.py
@@ -2,6 +2,9 @@ import pandas as pd
 import keras
 import trip_data_parser
 import sequence_creator
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
 
 trip_data_file = "data/trip_data_jan_sept_2017_reduced.csv"
 weather_data_file = "data/weather_jan_sept_2017.csv"
@@ -12,6 +15,29 @@ time_slot_size = 30 # minutes
 pick_ups_by_station = trip_data_parser.get_pick_ups(trip_data_file,weather_data_file,
                                                     time_slot_size,new_york_holidays)
 
-test_station = pick_ups_by_station[72].copy()
+# We first perform experiments only on one test station
+station_df = pick_ups_by_station[72].copy()
 
-sequence_creator.convert_to_sequence(test_station,2)
+# For now, we just remove non-teporal variable "holiday"
+station_df.drop(columns=["holiday"],inplace=True)
+
+# For now, we encode categorical variable "condition" with integer encoding
+# Later we will use one-hot encoding
+label_encoder = LabelEncoder() # From sklearn package
+station_df.condition = label_encoder.fit_transform(station_df.condition.values)
+
+# Normalize all data
+min_max_scaler = MinMaxScaler()
+station_df = pd.DataFrame(min_max_scaler.fit_transform(station_df), columns=station_df.columns)
+
+# Frame data as a sequence
+# x_data is input, y_data is output
+lag = 15
+x_data, y_data = sequence_creator.convert_to_sequence(station_df,["pick_ups"],lag,1)
+
+# Convert data into np array and split into traning and test
+x_data_train, x_data_test, y_data_train, y_data_test = train_test_split(x_data, y_data, test_size=0.10, shuffle=False)
+
+# Reshape input to be 3D [samples, timesteps, features]
+x_data_train = x_data_train.values.reshape((x_data_train.shape[0], lag+1, x_data_train.shape[1]/(lag+1)))
+x_data_test = x_data_test.values.reshape((x_data_test.shape[0], lag+1, x_data_test.shape[1]/(lag+1)))
--- sequence_creator.py
+++ sequence_creator.py
@@ -1,12 +1,26 @@
 import pandas as pd
 
-def convert_to_sequence(df, lags=1, aheads=0, dropnan=True):
-    columns = list(df.columns.values)
-    for lag in range(1, lags+1, 1):
-        col_idx = 0
-        for column in columns:
-            df.insert(loc=col_idx, column=column+"_lag_"+str(lag), value=df[column].shift(lag))
-            col_idx += 1
+def convert_to_sequence(df, output_columns, lags=0, aheads=1, dropnan=True):
+    new_df = pd.DataFrame()
+    x_columns = []
+    # Add lags (t-lag, t-lag+1, t-lag+2, ... , t-1)
+    for lag in range(lags, 0, -1):
+        for column in df.columns:
+            new_column_name = column+"_lag_"+str(lag)
+            new_df[new_column_name] = df[column].shift(lag)
+            x_columns.append(new_column_name)
+    # Add current observation (t)
+    for column in df.columns:
+        new_df[column] = df[column].values
+        x_columns.append(column)
+    # Add ste aheads (t+1, t+2, ... , t+aheads)
+    y_columns = []
     for ahead in range(1, aheads+1, 1):
-        for column in columns:
-            df[column+"_ahead_"+str(ahead)] = df[column].shift(-ahead)
+        for output_column in output_columns:
+            new_column_name = output_column+"_ahead_"+str(ahead)
+            new_df[new_column_name] = df[output_column].shift(-ahead)
+            y_columns.append(new_column_name)
+    if dropnan:
+        new_df.dropna(inplace=True)
+    
+    return [new_df[x_columns], new_df[y_columns]]
